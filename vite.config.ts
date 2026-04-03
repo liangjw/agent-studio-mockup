2e18b7d4270bb5de2b5b8b161f77cdc1a4edc8cc
liangjw <fliedwolf@gmail.com>
2026-04-03 10:06:00 +0800

    用于触发action运行

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    
     base: '/agent-studio-mockup/', // GitHub Pages 子路径
     plugins: [react(), tailwindcss()],
     define: {
